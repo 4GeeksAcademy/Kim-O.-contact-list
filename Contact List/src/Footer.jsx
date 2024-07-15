4eb77b3a1a21e05195c2a5e9c0f1c36d8893464d
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-07-15 01:05:46 +0000

    working

---
+++ Contact List/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+    return(
+       <footer className="footer">
+            <p>
+                Made with <i className="fa fa-heart text-danger" /> by Kim O.
+                <a href="#">Kim Ornis</a>
+            </p>
+       </footer>
+    );
+
+}
+export default Footer
---
+++ Contact List/src/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header(){
+    return (
+        <Header className="headerContainer">
+            <div> 
+                <h3>Kim's Contact List</h3>
+            </div>
+            <div className="newContactLink">
+                <h2>Add New Contact</h2>
+          </div>
+        </Header>
+      );    
+}
+export default Header
