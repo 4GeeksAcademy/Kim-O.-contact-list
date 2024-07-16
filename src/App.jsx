da2ee7df08701031584dc1757b75ddf336179a57
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-07-16 01:20:19 +0000

    working 7.15

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
+import ContactCard from './ContactCard.jsx'
+
+function App(){
+    return (
+      <>
+        <Header/>
+        <ContactCard/>
+        <Footer/>
+      </>
+    );
+  }
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header(){
+    return (
+        <nav className="headerContainer">
+            <button type="button" className="headerButton"> 
+                <a href="#">Kim's Contact List</a>
+            </button>
+            <button type="button" className="headerButton1">
+                <a href="#">Add New Contact</a>
+            </button>
+        </nav>
+      );    
+}
+export default Header
+
