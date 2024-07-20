be87626b992b91a4002c70220231e5ff41c79685
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-07-20 00:11:20 +0000

    corrections 7.19

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -24,23 +24,23 @@ function ContactCard(){
     <div className="contact-card col-xl-4 col-sm-6 mb-3">
         <div className="bg-white rounded py-4 px-4 contact-card-border">
             <div className="text-center">
-                <label for="Name" id="exampleInputHeader">First and Last Name </label><br></br>
-                <input type="name" class="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Enter email"></input><br></br>
+                <label form="Name" id="exampleInputHeader">First and Last Name </label><br></br>
+                <input type="name" className="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Enter email"></input><br></br>
             </div><br></br>
         
             <div className="row mt-2  icons-margin-left ">
                 <div className="col-12">
                     <div className="d-flex mb-2">
-                        <label for="exampleInputEmail" id="exampleInputHeader">Email address</label><br></br>
-                        <input type="email" class="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Please enter your email"></input><br></br>
+                        <label form="exampleInputEmail" id="exampleInputHeader">Email address</label><br></br>
+                        <input type="email" className="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Please enter your email"></input><br></br>
                     </div><br></br>
                     <div className="d-flex mb-2">
-                        <label for="exampleInputAddress" id="exampleInputHeader">Address</label><br></br>
-                        <input type="address" class="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Please enter your Address"></input><br></br>
+                        <label form="exampleInputAddress" id="exampleInputHeader">Address</label><br></br>
+                        <input type="address" className="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Please enter your Address"></input><br></br>
                     </div><br></br>
                     <div className="d-flex mb-2">
-                        <label for="exampleInputPhone" id="exampleInputHeader">Phone</label><br></br>
-                        <input type="phone" class="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Please Enter your Phone Number"></input><br></br>
+                        <label form="exampleInputPhone" id="exampleInputHeader">Phone</label><br></br>
+                        <input type="phone" className="form-control" id="exampleInput" aria-describedby="emailHelp" placeholder="Please Enter your Phone Number"></input><br></br>
                     </div>
                 </div>
             </div><br></br>
